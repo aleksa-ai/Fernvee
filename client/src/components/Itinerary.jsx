321f3098c21592f0584f7a32e30cf125d98ace8c
Irena-Pod <irena.podgurski@gmail.com>
2021-02-15 20:54:57 +0000

    Update DayList component

--- client/src/components/Itinerary.jsx
+++ client/src/components/Itinerary.jsx
@@ -1,7 +1,32 @@
-import DayList from "./DayList"
+import { makeStyles } from "@material-ui/core/styles";
+import Button from '@material-ui/core/Button';
+import Input from "@material-ui/core/Input";
+
+import DayList from "./DayList";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 export default function Itinerary(props) {
+  const classes = useStyles();
   return (
-  <DayList/>
-  )
+    <>
+      <h1>Create My Trip</h1>
+      <form className={classes.root} noValidate autoComplete="off">
+        <Input
+          placeholder="Enter Trip Name"
+          inputProps={{ "aria-label": "description" }}
+        />
+        <Button variant="contained" color="secondary">
+          SAVE
+        </Button>
+      </form>
+      <DayList />
+    </>
+  );
 }
