908428b311131d7c13def5c77bf3685035226704
Irena-Pod <irena.podgurski@gmail.com>
2021-02-18 16:16:44 -0500

    Add UserTrips backend route and DB helper

--- back-end/routes/userTrips.js
+++ back-end/routes/userTrips.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-
 module.exports = ({
-  getUsers
+  getUserTrips
 }) => {
-  router.get('/', (req, res) => {
-    console.log(getUsers)
-    getUsers()
+  router.get('/:id', (req, res) => {
+    const placeId = req.params.id;
+    console.log(getUserTrips)
+    getUserTrips()
       .then((users) => res.json(users))
       .catch((err) => res.json({
         error: err.message
       }));
-  })
+  });
+
   return router;
 }
