5fe484a577c5bed450dce46122563d782cec1eda
Aleksa Gavrilovic <lks_g@live.ca>
2021-02-20 20:23:08 -0500

    Refactored the GET/POST with the helpers

--- back-end/helpers/dbhelpers.js
+++ back-end/helpers/dbhelpers.js
@@ -10,6 +10,36 @@ module.exports = (db) => {
       .catch((err) => err);
   };
 
+  const addUser = (
+    firstName,
+    lastName,
+    email,
+    password,
+    travelStyle,
+    createdAt
+  ) => {
+    const query = {
+      text: `INSERT INTO users(first_name, last_name, email, password, travel_style, created_at)
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING *Î;
+   `,
+    };
+
+    const values = [
+      firstName,
+      lastName,
+      email,
+      password,
+      travelStyle,
+      createdAt,
+    ];
+
+    return db
+      .query(query, values)
+      .then((result) => result.rows)
+      .catch((err) => err);
+  };
+
   const getUserTrips = (userId) => {
     const query = {
       text: `
@@ -124,6 +154,7 @@ module.exports = (db) => {
 
   return {
     getUsers,
+    addUser,
     getActivities,
     getActivityCategories,
     getPlannedActivities,
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 module.exports = ({
-  getUsers
+  getUsers, addUser
 }) => {
   router.get('/', (req, res) => {
     console.log(getUsers)
@@ -12,16 +12,14 @@ module.exports = ({
       .catch((err) => res.json({
         error: err.message
       }));
-  })
+  });
 
   router.post("/", (req, res) => {
-    db.query(
-      `INSERT INTO users (first_name, last_name, email, password, travel_style, created_at)
-       VALUES ($1::text, $2::text, $3::text, $4::text, $5::text, $6::text)
-       RETURNING id;
-    `,
-      [req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.travel_style, req.body.created_at]
-    )
+    addUser()
+    .then((user) => res.json(user))
+      .catch((err) => res.json({
+        error: err.message
+      }));
   });
 
   return router;
