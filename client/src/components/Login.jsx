2604220e72fc8c214c9a4d094dbf60b44b7e512c
Aleksa Gavrilovic <lks_g@live.ca>
2021-02-21 00:16:29 -0500

    Attempt at iplementing cookie to hold user info. For doesn't submit data

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,58 +1,62 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
-import { useCookies } from 'react-cookie';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { useCookies } from "react-cookie";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { da } from "date-fns/locale";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Fernvee, Inc. All rights reserved
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100vh'
+    height: "100vh",
   },
   image: {
-    backgroundImage: 'url(https://images.unsplash.com/photo-1585224489225-03ca294de508?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=3634&q=80)',
-    backgroundRepeat: 'no-repeat',
+    backgroundImage:
+      "url(https://images.unsplash.com/photo-1585224489225-03ca294de508?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=3634&q=80)",
+    backgroundRepeat: "no-repeat",
     backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
   },
   paper: {
     margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -60,8 +64,43 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignInSide() {
+export default function SignInSide(props) {
+  const [cookies, setCookie] = useCookies(["name", "id"]);
+  const { register, handleSubmit } = useForm();
+
   const classes = useStyles();
+
+  const onSubmit = (data) => {
+    console.log('LOGIN ONSUBMIT DATA', data)
+    // let email = data.email;
+    // let password = data.password;
+    // Promise.all([axios.get("/api/users")])
+    //   .then((all) => {
+    //     console.log('LOGIN ALL!!!!!', all)
+    //     const users = all[0].data.users;
+    //     const filteredUser = users.filter(
+    //       (filteredUser) => filteredUser.email === email
+    //     )[0];
+    //     if (!filteredUser) {
+    //       console.log("Wrong email");
+    //       return;
+    //     }
+    //     if (filteredUser.password === password) {
+    //       console.log(filteredUser, "in");
+    //       setCookie("name", filteredUser.firstname);
+    //       setCookie("id", filteredUser.id);
+    //       return filteredUser;
+    //     } else if (filteredUser && filteredUser.password !== password) {
+    //       console.log("Wrong password");
+    //       return null;
+    //     }
+    //     console.log("Wrong");
+    //     return null;
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+  };
 
   return (
     <Grid container component="main" className={classes.root}>
@@ -75,7 +114,7 @@ export default function SignInSide() {
           <Typography component="h1" variant="h5">
             Login
           </Typography>
-          <form className={classes.form} noValidate>
+          <form className={classes.form} noValidate onSubmit={handleSubmit(onSubmit)}>
             <TextField
               variant="outlined"
               margin="normal"
@@ -86,6 +125,9 @@ export default function SignInSide() {
               name="email"
               autoComplete="email"
               autoFocus
+              type="text"
+              id="email"
+              ref={ register }
             />
             <TextField
               variant="outlined"
@@ -96,6 +138,7 @@ export default function SignInSide() {
               label="Password"
               type="password"
               id="password"
+              ref={ register }
               autoComplete="current-password"
             />
             <FormControlLabel
