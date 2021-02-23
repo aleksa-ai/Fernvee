4430c9f593a5342a5ce7a2028f0610cde2293b16
Aleksa Gavrilovic <lks_g@live.ca>
2021-02-22 21:28:35 -0500

    Update footer style

--- client/src/components/Partials/_Footer/_index.jsx
+++ client/src/components/Partials/_Footer/_index.jsx
@@ -4,15 +4,28 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   stickToBottom: {
     position: "fixed",
     bottom: 0,
     width: "100%",
-
+    justifyContent: "flex-end",
   },
 });
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit">Fernvee, Inc. All rights reserved</Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 export default function Footer(props) {
   const classes = useStyles();
@@ -20,17 +33,21 @@ export default function Footer(props) {
 
   return (
     <BottomNavigation
-    value={value}
-    onChange={(event, newValue) => {
-      setValue(newValue);
-    }}
-    showLabels
-    className={classes.stickToBottom} 
-  >
-  
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.stickToBottom}
+    >
+      {/* <Copyright className={classes.stickToBottom}/> */}
       <BottomNavigationAction label="About Us" component={Link} to="/AboutUs" />
       <BottomNavigationAction label="Blog" component={Link} to="/Blog" />
-      <BottomNavigationAction label="Contact Us" component={Link} to="/ContactUs" />
+      <BottomNavigationAction
+        label="Contact Us"
+        component={Link}
+        to="/ContactUs"
+      />
     </BottomNavigation>
   );
 }
