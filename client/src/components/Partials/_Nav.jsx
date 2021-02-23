4aea9c32b3b9315aa63f60d877e8220793217d35
Aleksa Gavrilovic <lks_g@live.ca>
2021-02-22 20:44:38 -0500

    Make nav bar more responsive

--- client/src/components/Partials/_Nav.jsx
+++ client/src/components/Partials/_Nav.jsx
@@ -9,48 +9,85 @@ import {
 import { AppBar, Toolbar, Tab, Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-import ExploreIcon from '@material-ui/icons/Explore';
-import MapIcon from '@material-ui/icons/Map';
-import CreateIcon from '@material-ui/icons/Create';
-import AccountCircleSharpIcon from '@material-ui/icons/AccountCircleSharp';
-import LockSharpIcon from '@material-ui/icons/LockSharp';
-import LockOpenSharpIcon from '@material-ui/icons/LockOpenSharp';
-import AssignmentTurnedInSharpIcon from '@material-ui/icons/AssignmentTurnedInSharp';
+import ExploreIcon from "@material-ui/icons/Explore";
+import MapIcon from "@material-ui/icons/Map";
+import CreateIcon from "@material-ui/icons/Create";
+import AccountCircleSharpIcon from "@material-ui/icons/AccountCircleSharp";
+import LockSharpIcon from "@material-ui/icons/LockSharp";
+import LockOpenSharpIcon from "@material-ui/icons/LockOpenSharp";
+import AssignmentTurnedInSharpIcon from "@material-ui/icons/AssignmentTurnedInSharp";
 
-import { useCookies } from "react-cookie"
+import { useCookies } from "react-cookie";
 
+import "./_Nav.scss";
 
 const useStyles = makeStyles({
   stickToTop: {
-    minHeight: '80px'
- 
+    minHeight: "80px",
   },
 });
-
-
 
 export default function Nav(props) {
   const classes = useStyles();
   const [cookies, removeCookie] = useCookies();
 
-  console.log("USER NAME", cookies.name, "USER ID", cookies.id, "COOKIE", cookies)
+  console.log(
+    "USER NAME",
+    cookies.name,
+    "USER ID",
+    cookies.id,
+    "COOKIE",
+    cookies
+  );
 
   const logout = (cookies) => {
-    removeCookie("id")
-    removeCookie("name")
-    window.location = "/" 
-  }
+    removeCookie("id");
+    removeCookie("name");
+    window.location = "/";
+  };
 
   return (
-    <AppBar position="fixed" >
+    <AppBar position="fixed">
       <Toolbar className={classes.stickToTop}>
-        <a href="/"><img src="../images/logo.png" alt="logo" width="70%" /></a>
-        <Tab label="Explore" icon={<ExploreIcon/>} component={NavLink} to="/" /> 
-        <Tab label="Trips" icon={<MapIcon/>} component={NavLink} to="/trips" />
-        <Tab label="Create" icon={<CreateIcon/>} component={NavLink} to="/create" />
-        <Tab label="My Profile" icon={<AccountCircleSharpIcon/>} component={NavLink} to="/MyProfile" />
-        {cookies.id === "undefined" ? <Tab label="Login" icon={<LockSharpIcon/>} component={NavLink} to="/login" /> : <Tab label="Logout"  icon={<LockOpenSharpIcon/>} onClick={logout}/>}
-        <Tab label="Signup" icon={<AssignmentTurnedInSharpIcon/>} component={NavLink} to="/signup" />
+        <div class="main-nav-buttons">
+           <a href="/">
+              <img src="../images/logo.png" class="logo" alt="logo"/>
+          </a>
+          <Tab
+            label="Explore"
+            icon={<ExploreIcon fontSize="large" />}
+            component={NavLink}
+            to="/"
+          />
+          <Tab
+            label="Trips"
+            icon={<MapIcon fontSize="large" />}
+            component={NavLink}
+            to="/trips"
+          />
+          <Tab
+            label="Create"
+            icon={<CreateIcon fontSize="large" />}
+            component={NavLink}
+            to="/create"
+          />
+        </div>
+        {/* <Tab label="My Profile" icon={<AccountCircleSharpIcon/>} component={NavLink} to="/MyProfile" /> */}
+        {cookies.id === "undefined" ? (
+          <Tab
+            label="Login"
+            icon={<LockSharpIcon fontSize="large" />}
+            component={NavLink}
+            to="/login"
+          />
+        ) : (
+          <Tab
+            label="Logout"
+            icon={<LockOpenSharpIcon fontSize="large" />}
+            onClick={logout}
+          />
+        )}
+        {/* <Tab label="Signup" icon={<AssignmentTurnedInSharpIcon/>} component={NavLink} to="/signup" /> */}
       </Toolbar>
     </AppBar>
   );
