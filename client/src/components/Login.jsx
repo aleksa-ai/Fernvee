bd061075f73033083af1cfe94194e91d5d20c417
Aleksa Gavrilovic <lks_g@live.ca>
2021-02-24 00:30:48 -0500

    Fix sign up links

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -163,7 +163,7 @@ export default function Login(props) {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
+                <Link href="/signup" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
